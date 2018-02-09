80171bfba8540331f02100e527f3f72f8410a4ca
bzabza <akkdlyapereliva@mail.ru>
2018-02-09 20:53:10 +0200

    restyle homepage

--- src/client/app/containers/home-page/index.js
+++ src/client/app/containers/home-page/index.js
@@ -7,10 +7,12 @@ class HomePage extends React.Component {
     render() {
         return (
             <section className="containers">
-                <h1>Hello {this.props.userData.username}</h1>
-                <div className=" home-content col-md-12 d-flex flex-wrap justify-content-between">
-                    <div><Sales/></div>
-                    <div><Report/></div>
+                <div className="container">
+                    <h1>Hello {this.props.userData.username}!</h1>
+                    <div className=" home-content col-md-12 d-flex flex-wrap justify-content-between">
+                        <div><Sales/></div>
+                        <div><Report/></div>
+                    </div>
                 </div>
             </section>
         )
